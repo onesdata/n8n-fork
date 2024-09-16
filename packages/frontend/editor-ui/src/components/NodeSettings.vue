d81b1a087b5e22d764b682cf72a3b7fdf01d0f46
Adrián Martínez Guantes <adrian.martinez@onestic.com>
2024-09-16 11:54:27 +0200

    Raise retry max values: maxTries=100 (was 5), waitBetweenTries=60000 (ms) (was 5000)

--- packages/frontend/editor-ui/src/components/NodeSettings.vue
+++ packages/frontend/editor-ui/src/components/NodeSettings.vue
@@ -650,7 +650,7 @@ const populateSettings = () => {
 					type: 'number',
 					typeOptions: {
 						minValue: 2,
-						maxValue: 5,
+						maxValue: 100,
 					},
 					default: 3,
 					displayOptions: {
@@ -667,7 +667,7 @@ const populateSettings = () => {
 					type: 'number',
 					typeOptions: {
 						minValue: 0,
-						maxValue: 5000,
+						maxValue: 60000,
 					},
 					default: 1000,
 					displayOptions: {
