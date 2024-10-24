bf7a2bcf23404ed0181fa067e4cf730b8f30ef74
Adrián Martínez Guantes <adrian.martinez@onestic.com>
2024-10-24 21:36:32 +0200

    Allow visual recognition of different stages via sidebar icons and favicon

--- packages/editor-ui/src/components/MainSidebar.vue
+++ packages/editor-ui/src/components/MainSidebar.vue
@@ -167,7 +167,7 @@ const mainMenuItems = computed(() => [
 const isCollapsed = computed(() => uiStore.sidebarMenuCollapsed);
 
 const logoPath = computed(
-	() => basePath.value + (isCollapsed.value ? 'static/logo/collapsed.svg' : uiStore.logo),
+	() => basePath.value + (isCollapsed.value ? `static/logo/collapsed-${settingsStore.settings.aitProjectStage}.svg` : uiStore.logo),
 );
 
 const hasVersionUpdates = computed(
@@ -408,7 +408,7 @@ const checkWidthAndAdjustSidebar = async (width: number) => {
 		img {
 			position: relative;
 			left: 1px;
-			height: 20px;
+			max-height: 48px;
 		}
 	}
 
