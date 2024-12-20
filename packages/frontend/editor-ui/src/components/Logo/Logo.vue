1a076b1273d1f2b8409138130646cb4acc0c5b9a
Adrián Martínez Guantes <adrian.martinez@onestic.com>
2024-12-20 12:18:46 +0100

    Allow visual recognition of different stages via sidebar icons and favicon

--- packages/frontend/editor-ui/src/components/Logo/Logo.vue
+++ packages/frontend/editor-ui/src/components/Logo/Logo.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import type { FrontendSettings } from '@n8n/api-types';
 import { computed, onMounted, useCssModule, useTemplateRef } from 'vue';
+import { useSettingsStore } from '@/stores/settings.store';
 import { useFavicon } from '@vueuse/core';
 
 import LogoIcon from './logo-icon.svg';
 import LogoText from './logo-text.svg';
+
+const settingsStore = useSettingsStore();
 
 const props = defineProps<
 	(
@@ -21,9 +24,9 @@ const props = defineProps<
 >();
 
 const { location, releaseChannel } = props;
+const { aitProjectStage } = settingsStore.settings;
 
-const showReleaseChannelTag = computed(() => {
-	if (releaseChannel === 'stable') return false;
+const showAitProjectStageTag = computed(() => {
 	if (location === 'authView') return true;
 	return !props.collapsed;
 });
@@ -47,13 +50,31 @@ const containerClasses = computed(() => {
 
 const svg = useTemplateRef<{ $el: Element }>('logo');
 onMounted(() => {
-	if (releaseChannel === 'stable' || !('createObjectURL' in URL)) return;
+	if (!('createObjectURL' in URL)) return;
 
 	const logoEl = svg.value!.$el;
 
 	// Change the logo fill color inline, so that favicon can also use it
-	const logoColor = releaseChannel === 'dev' ? '#838383' : '#E9984B';
-	logoEl.querySelector('path')?.setAttribute('fill', logoColor);
+	let logoColor;
+
+	switch (aitProjectStage) {
+		case 'local':
+			logoColor = '#0015ff';
+			break;
+		case 'test':
+			logoColor = '#49d204';
+			break;
+		case 'staging':
+			logoColor = '#fdd404';
+			break;
+		case 'prod':
+			logoColor = '#fa2602';
+			break;
+	}
+
+	if (logoColor) {
+		logoEl.querySelector('path')?.setAttribute('fill', logoColor);
+	}
 
 	// Reuse the SVG as favicon
 	const blob = new Blob([logoEl.outerHTML], { type: 'image/svg+xml' });
@@ -65,7 +86,7 @@ onMounted(() => {
 	<div :class="containerClasses" data-test-id="n8n-logo">
 		<LogoIcon ref="logo" :class="$style.logo" />
 		<LogoText v-if="showLogoText" :class="$style.logoText" />
-		<div v-if="showReleaseChannelTag" :class="$style.releaseChannelTag">{{ releaseChannel }}</div>
+		<div v-if="showAitProjectStageTag" :class="$style.aitProjectStageTag">{{ aitProjectStage }}</div>
 		<slot />
 	</div>
 </template>
@@ -84,7 +105,7 @@ onMounted(() => {
 	}
 }
 
-.releaseChannelTag {
+.aitProjectStageTag {
 	color: var(--color-text-dark);
 	padding: var(--spacing-5xs) var(--spacing-4xs);
 	background-color: var(--color-background-base);
