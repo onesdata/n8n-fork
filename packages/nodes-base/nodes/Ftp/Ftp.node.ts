14efc6afccaacf60245b03d2a860fe5e3c083553
Adrián Martínez Guantes <adrian.martinez@onestic.com>
2025-01-08 16:59:08 +0100

    Adapt FTPS solution from https://github.com/morphar/n8n/pull/1/files

--- packages/nodes-base/nodes/Ftp/Ftp.node.ts
+++ packages/nodes-base/nodes/Ftp/Ftp.node.ts
@@ -19,6 +19,7 @@ import sftpClient from 'ssh2-sftp-client';
 import type { Readable } from 'stream';
 import { pipeline } from 'stream/promises';
 import { file as tmpFile } from 'tmp-promise';
+import { ConnectionOptions } from 'tls';
 
 import { formatPrivateKey, generatePairedItemData } from '@utils/utilities';
 
@@ -512,7 +513,17 @@ export class Ftp implements INodeType {
 						operation: ['list'],
 					},
 				},
-				options: [timeoutOption],
+				options: [
+					timeoutOption,
+					{
+						displayName: 'Reject Unauthorized',
+						name: 'rejectUnauthorized',
+						type: 'boolean',
+						default: false,
+						description:
+							'Whether the server certificate is verified against the list of supplied CAs',
+					}
+				],
 			},
 		],
 	};
@@ -531,6 +542,13 @@ export class Ftp implements INodeType {
 						port: credentials.port as number,
 						user: credentials.username as string,
 						password: credentials.password as string,
+						secure: credentials.secure as string | boolean,
+						secureOptions: (credentials.secure
+							? {
+								requestCert: credentials.disableCertificateValidation,
+								rejectUnauthorized: !credentials.disableCertificateValidation,
+							}
+							: undefined) as ConnectionOptions | undefined,
 					});
 				} catch (error) {
 					await ftp.end();
@@ -640,6 +658,7 @@ export class Ftp implements INodeType {
 						user: credentials.username as string,
 						password: credentials.password as string,
 						connTimeout: connectionTimeout,
+						secure: credentials.secure as boolean,
 					});
 				}
 			} catch (error) {
