9bdf46506ca1ec31006599918f193231e9671a1c
Adrián Martínez Guantes <adrian.martinez@onestic.com>
2025-06-20 12:45:33 +0200

    fix(BigQuery Node): Fix "Maximum call stack size exceeded" error when handling a large number of rows See https://github.com/n8n-io/n8n/pull/10965/files

--- packages/nodes-base/nodes/Google/BigQuery/v2/actions/database/executeQuery.operation.ts
+++ packages/nodes-base/nodes/Google/BigQuery/v2/actions/database/executeQuery.operation.ts
@@ -224,7 +224,7 @@ export async function execute(this: IExecuteFunctions): Promise<INodeExecutionDa
 	const items = this.getInputData();
 	const length = items.length;
 
-	const returnData: INodeExecutionData[] = [];
+	let returnData: INodeExecutionData[] = [];
 
 	let jobs = [];
 	let maxResults = 1000;
@@ -371,7 +371,7 @@ export async function execute(this: IExecuteFunctions): Promise<INodeExecutionDa
 					}
 				}
 
-				returnData.push(...prepareOutput.call(this, queryResponse, i, raw, includeSchema));
+				returnData = returnData.concat(prepareOutput.call(this, queryResponse, i, raw, includeSchema));
 			} else {
 				jobs.push({ jobId, projectId, i, raw, includeSchema, location });
 			}
@@ -381,7 +381,7 @@ export async function execute(this: IExecuteFunctions): Promise<INodeExecutionDa
 					this.helpers.returnJsonArray({ error: error.message }),
 					{ itemData: { item: i } },
 				);
-				returnData.push(...executionErrorData);
+				returnData = returnData.concat(executionErrorData);
 				continue;
 			}
 			if ((error.message as string).includes('location') || error.httpCode === '404') {
@@ -427,7 +427,7 @@ export async function execute(this: IExecuteFunctions): Promise<INodeExecutionDa
 				if (response.jobComplete) {
 					settledJobs.push(job.jobId);
 
-					returnData.push(...prepareOutput.call(this, response, job.i, job.raw, job.includeSchema));
+					returnData = returnData.concat(prepareOutput.call(this, response, job.i, job.raw, job.includeSchema));
 				}
 				if ((response?.errors as IDataObject[])?.length) {
 					const errorMessages = (response.errors as IDataObject[]).map((error) => error.message);
@@ -446,7 +446,7 @@ export async function execute(this: IExecuteFunctions): Promise<INodeExecutionDa
 						this.helpers.returnJsonArray({ error: error.message }),
 						{ itemData: { item: job.i } },
 					);
-					returnData.push(...executionErrorData);
+					returnData = returnData.concat(executionErrorData);
 					continue;
 				}
 				throw new NodeOperationError(this.getNode(), error as Error, {
