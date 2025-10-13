cf1d1f922bedd8f0aa2ac6a8b5530a4ff9ff2c47
Adrián Martínez Guantes <adrian.martinez@onestic.com>
2025-10-13 10:35:12 +0200

    Update logo files to match n8n updates

--- packages/frontend/editor-ui/src/app/components/MainSidebarHeader.vue
+++ packages/frontend/editor-ui/src/app/components/MainSidebarHeader.vue
@@ -70,6 +70,7 @@ const {
 				size="small"
 				:collapsed="isCollapsed"
 				:release-channel="settingsStore.settings.releaseChannel"
+				:ait-project-stage="settingsStore.settings.aitProjectStage"
 			>
 				<N8nTooltip
 					v-if="sourceControlStore.preferences.branchReadOnly && !isCollapsed"
